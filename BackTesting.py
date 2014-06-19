acd64530a77b0bf2c06ab5965199c83b9dedf383
elsdrm <elsdrm@gmail.com>
2014-06-19 15:57:50 +0800

    stock historical-data included

---
+++ BackTesting.py
@@ -0,0 +1,14 @@
+import datetime
+from RawDataProcessing import *
+
+def StrategyTesting(beginDate, endDate, strategy, optionData=None, stockData=None):
+	beginDate = datetime.datetime.strptime( beginDate, '%Y/%m/%d' ).date()
+	endDate   = datetime.datetime.strptime( endDate,   '%Y/%m/%d' ).date()
+	dtIdx = OptionDailyData.invKeys['Date']
+
+	dataList = [optionData, stockData]
+	for data in dataList:
+		if data[dtIdx] < beginDate: continue
+		if data[dtIdx] > endDate:   break
+
+		# not finish
