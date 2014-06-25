0fb4093586a3a4a1e15200192cbd68018557943b
elsdrm <elsdrm@gmail.com>
2014-06-25 16:24:48 +0800

    QQ

--- main.py
+++ main.py
@@ -5,23 +5,26 @@ from matplotlib import dates
 import datetime
 import numpy as np
 
-# ############################# Constants ##############################
+############################## Constants ##############################
 beginDate = str2date('2003/07/01')
-endDate = str2date('2013/12/31')
+endDate = str2date('2003/10/31')
 maturity = str2date('2012/08/01')
 
 kwargs = {
 	'Type': 'Call',
-#	'Strike': 8000,
-#	'Maturity': maturity
+	# 'Strike': 8000,
+	# 'Maturity': maturity,
 }
 
-stockTransCost = 0.4
-futuresTransCost = 200
-optionTransCost = 50
+outOfMoney = 100
 
-# ############################ Loading Data ############################
+stockTransCost = 0.2
+futuresTransCost = 0
+optionTransCost = 0.8
+
+############################# Loading Data ############################
 try:
+	# no needs to reload data every time
 	len(option_data)
 except NameError:
 	sdd = StockDailyData()
@@ -46,7 +49,7 @@ except NameError:
 	option_data = odd.getDataByDate(beginDate, endDate, **kwargs)
 	print( '......Done.' )
 
-############################ Preprocessing ############################
+	# ########################### Preprocessing ############################
 	dateSequence = np.array([r[sdd.invKeys['Date']] for r in stock_data])
 
 	stockCloseIdx = sdd.invKeys['Close']
@@ -58,7 +61,7 @@ except NameError:
 	stockPrice = np.array([r[stockCloseIdx] for r in stock_data])
 	indexPrice = np.array([r[indexCloseIdx] for r in index_data])
 	futuresPrice = np.array([r[futuresCloseIdx] for r in futures_data])
-	optionPrice  = np.array([ r[optionCloseIdx ] for r in option_data  ])
+	#optionPrice = np.array([r[optionCloseIdx] for r in option_data])
 
 	stockReturn = returnRate(stockPrice)
 	indexReturn = returnRate(indexPrice)
@@ -67,47 +70,108 @@ except NameError:
 # end of try-except scope
 
 ########################### Testing Strategy ##########################
-positionLimit = {'0050': 50, }
-TransactionCost = {'0050': stockTransCost, }
+opdata = []
+def selectOptionPrice():
+	optionMaturityIdx  = odd.invKeys['Maturity']
+	optionStrikeIdx    = odd.invKeys['Strike']
+	optionDateIdx      = odd.invKeys['Date']
+	futuresMaturityIdx = fdd.invKeys['Maturity']
+	futuresDateIdx     = fdd.invKeys['Date']
+
+	result = []
+	i = 0
+	for f in futures_data:
+		while i != len(option_data):
+			if option_data[i][optionDateIdx] < f[futuresDateIdx]:
+				i += 1
+				continue
+
+			if option_data[i][optionMaturityIdx] == f[futuresMaturityIdx] and option_data[i][optionStrikeIdx] - f[futuresCloseIdx] >= outOfMoney:
+				result.append( option_data[i][optionCloseIdx] )
+				opdata.append( option_data[i] )
+				i += 1
+				break
+			else:
+				i += 1
+
+	return result
+
+
+optionPrice = selectOptionPrice()
+
+positionLimit = {'0050': 50,
+                 'TXO': 50,
+                 'TX': 50,
+}
+TransactionCost = {'0050': stockTransCost,
+                   'TXO': optionTransCost,
+                   'TX': futuresTransCost,
+}
+
+portfolio      = {'0050': 0, 'TXO': 0, 'TX': 0}
+pendingToTrade = {'0050': 0, 'TXO': 0, 'TX': 0}
+priceDifference = {'0050': np.append([0], np.diff(stockPrice)),
+                   'TXO': np.append([0], np.diff(optionPrice)),
+                   'TX': np.append([0], np.diff(futuresPrice)),
+}
 
 transactionLog = {}
 PL = [0]
 
-slow, fast, macd = movingAverageConvergence(stockPrice)
-portfolio = {'0050': 0, }
-pendingToTrade = {'0050': 0, }
 
-stockDiff = np.append([0], np.diff(stockPrice))
+# slow, fast, macd = movingAverageConvergence(stockPrice)
+randomNumber = np.round(np.random.lognormal() * 100)
+
 for i in range(len(indexPrice)):
+
+	### check maturity
+	todayMaturity = False
+	if i + 1 < len(indexPrice) and futures_data[i + 1][fdd.invKeys['Maturity']] != futures_data[i][
+		fdd.invKeys['Maturity']]:
+		todayMaturity = True
 
 	### update profit & loss
 	PL.append(PL[-1])
 	for item in portfolio:
-		PL[-1] += (stockDiff[i] * portfolio[item])
+		PL[-1] += (priceDifference[item][i] * portfolio[item])
 
 	### check trading signals
-	if macd[i] > 1:
-		pendingToTrade['0050'] += 1
-	if macd[i] < -1:
-		pendingToTrade['0050'] -= 1
+	if i == randomNumber:
+		#pendingToTrade['0050'] += 5000
+		pendingToTrade['TXO'] -= 50
+
+	if i >= randomNumber:
+		if portfolio['TXO'] == 0:
+			pendingToTrade['TXO'] -= 50
+
+	### check futures / option matures
+	if todayMaturity:
+		pendingToTrade['TXO'] = -portfolio['TXO']
+		pendingToTrade['TX']  = -portfolio['TX']
 
 	### validate transaction
 	for item in pendingToTrade:
 		if pendingToTrade[item] > 0 and portfolio[item] >= positionLimit[item]:
 			pendingToTrade[item] = 0
 
-		if pendingToTrade[item] < 0 and portfolio[item] <= positionLimit[item]:
+		if pendingToTrade[item] < 0 and -portfolio[item] >= positionLimit[item]:
 			pendingToTrade[item] = 0
 
 	### execute transaction
 	transactionLog[dateSequence[i]] = {}
 	for item in pendingToTrade:
 		if pendingToTrade[item] != 0:
+			print( str(dateSequence[i]) + ' : ' + item + ' ' + str(pendingToTrade[item]) )
+			print( '     Price : ' + str(optionPrice[i]) )
+			print( '     Data  : ' + str(opdata[i]) )
+			print( '  Futures  : ' + str(futuresPrice[i]) )
 			portfolio[item] += pendingToTrade[item]
 			PL[-1] -= (TransactionCost[item] * np.abs(pendingToTrade[item]))
 			transactionLog[dateSequence[i]][item] = pendingToTrade[item]
 			pendingToTrade[item] = 0
 
+PL = PL[1:]
+
 
 ########################## Generating Graphs ##########################
-
+pl.plot(dateSequence, PL)
