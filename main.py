1d3eabafdd7bfcfafae7ebbf29b844ac14358b38
elsdrm <elsdrm@gmail.com>
2014-08-07 13:08:53 +0800

    ...

--- main.py
+++ main.py
@@ -138,132 +138,133 @@ sellOptionPrice = [data[optionCloseIdx] for data in selectedSellOptionData]
 
 #plresult = []
 
-proportion = 0.8
-arg = [0.052]
-for R in arg:
-	positionLimit = {'0050': 10,
-	                 'TXO': 10,
-	                 'TX': 1000,
-	}
-	TransactionCost = {'0050': stockTransCost,
-	                   'TXO': optionTransCost,
-	                   'TX': optionTransCost,
-	}
+p_list = [0.8]
+R_list = [0.08]
+for proportion in p_list:
+	for R in R_list:
+		positionLimit = {'0050': 10,
+		                 'TXO': 10,
+		                 'TX': 1000,
+		}
+		TransactionCost = {'0050': stockTransCost,
+		                   'TXO': optionTransCost,
+		                   'TX': optionTransCost,
+		}
 
-	portfolio = {'0050': 0, 'TXO': 0, 'TX': 0}
-	pendingToTrade = {'0050': 0, 'TXO': 0, 'TX': 0}
-	priceDifference = {'0050': np.append([0], np.diff(stockPrice)),
-	                   'TXO': np.append([0] * (beginDateIndex + 1), np.diff(optionPrice)),
-	                   'TX': np.append([0] * (beginDateIndex + 1), np.diff(sellOptionPrice)),
-	}
+		portfolio = {'0050': 0, 'TXO': 0, 'TX': 0}
+		pendingToTrade = {'0050': 0, 'TXO': 0, 'TX': 0}
+		priceDifference = {'0050': np.append([0], np.diff(stockPrice)),
+		                   'TXO': np.append([0] * (beginDateIndex + 1), np.diff(optionPrice)),
+		                   'TX': np.append([0] * (beginDateIndex + 1), np.diff(sellOptionPrice)),
+		}
 
-	transactionLog = {}
-	PL = [0]
-	monthlyPL = [0]
-	maturities = []
+		transactionLog = {}
+		PL = [0]
+		monthlyPL = [0]
+		maturities = []
 
-	#R = 0.05
-	slow, fast, macd = movingAverageConvergence(indexPrice)
-	signal = movingAverage(macd, 9, type='exponential')
-	convergence = macd - signal
+		#R = 0.05
+		slow, fast, macd = movingAverageConvergence(indexPrice)
+		signal = movingAverage(macd, 9, type='exponential')
+		convergence = macd - signal
 
-	hedgeFlag = bool(0)
-	initFlag = bool(1)
-	shortFlag = bool(0)
-	sellOptionFlag = bool(1)
-
-
-	for i in range(beginDateIndex, len(indexPrice)):
-
-		# ## check maturity
-		todayMaturity = False
-		if i + 1 < len(indexPrice) and futures_data[i + 1][fdd.invKeys['Maturity']] != futures_data[i][
-			fdd.invKeys['Maturity']]:
-			todayMaturity = True
-
-		### update profit & loss
-		PL.append(PL[-1])
-		for item in portfolio:
-			PL[-1] += (priceDifference[item][i] * portfolio[item] * UNIT[item])
-
-		if dividend0050.has_key(len(PL) - 2):
-			PL[-1] += dividend0050[len(PL) - 2] * portfolio['0050'] * UNIT['0050']
-
-		################## check trading signals ###############################
-
-		# initial position
-		if initFlag:
-			initFlag = False
-			pendingToTrade['0050'] += 10
-
-		# 2 strategies
-		if hedgeFlag:
-			if (-macd[i]) > R * fast[i]:
-				pendingToTrade['TXO'] -= 10
-		else:
-			if (-macd[i]) > R * fast[i]:
-				pendingToTrade['TXO'] -= 10
-#				pendingToTrade['TX'] -= portfolio['TX']
-#				sellOptionFlag = False
-
-			sellAmount = 1
-
-			if (-macd[i]) > proportion * R * fast[i]:
-				pendingToTrade['TX'] -= sellAmount
-#				sellOptionFlag = True
-
-#			if todayMaturity and sellOptionFlag:
-#				sellOptionFlag = False
-
-			# stopping loss
-#			if macd[i] > 0:
-#				pendingToTrade['TX'] = -portfolio['TX']
-#				pendingToTrade['TXO'] = -portfolio['TXO']
+		hedgeFlag = bool(0)
+		initFlag = bool(1)
+		shortFlag = bool(0)
+		sellOptionFlag = bool(1)
 
 
-			#	if i > 0 and macd[i-1] < 0 and convergence[i - 1] > 0 and convergence[i] < 0:
-			#		pendingToTrade['TXO'] -= shortAmount
-			#		shortFlag = True
-			#
-			#	if i > 0 and macd[i-1] > 0 and convergence[i - 1] < 0 and convergence[i] > 0:
-			#		pendingToTrade['TXO'] -= shortAmount
-			#		shortFlag = True
+		for i in range(beginDateIndex, len(indexPrice)):
 
-			#	if i > 0 and convergence[i - 1] < 0 and convergence[i] > 0:
-			#		pendingToTrade['TXO'] -= portfolio['TXO']
-			#		shortFlag = False
+			# ## check maturity
+			todayMaturity = False
+			if i + 1 < len(indexPrice) and futures_data[i + 1][fdd.invKeys['Maturity']] != futures_data[i][
+				fdd.invKeys['Maturity']]:
+				todayMaturity = True
 
-		########################################################################
+			### update profit & loss
+			PL.append(PL[-1])
+			for item in portfolio:
+				PL[-1] += (priceDifference[item][i] * portfolio[item] * UNIT[item])
 
-		### check futures / option matures
-		if todayMaturity:
-			pendingToTrade['TXO'] = -portfolio['TXO']
-			pendingToTrade['TX'] = -portfolio['TX']
+			if dividend0050.has_key(len(PL) - 2):
+				PL[-1] += dividend0050[len(PL) - 2] * portfolio['0050'] * UNIT['0050']
 
-			priceDifference['TXO'][i + 1] = 0
-			priceDifference['TX'][i + 1] = 0
+			################## check trading signals ###############################
 
-		### validate transaction
-		for item in pendingToTrade:
-			if pendingToTrade[item] > 0 and portfolio[item] >= positionLimit[item]:
-				pendingToTrade[item] = 0
+			# initial position
+			if initFlag:
+				initFlag = False
+				pendingToTrade['0050'] += 10
 
-			if pendingToTrade[item] < 0 and -portfolio[item] >= positionLimit[item]:
-				pendingToTrade[item] = 0
+			# 2 strategies
+			if hedgeFlag:
+				if (-macd[i]) > R * fast[i]:
+					pendingToTrade['TXO'] -= 10
+			else:
+				if (-macd[i]) > R * fast[i]:
+					pendingToTrade['TXO'] -= 10
+	#				pendingToTrade['TX'] -= portfolio['TX']
+	#				sellOptionFlag = False
 
-		### execute transaction
-		transactionLog[dateSequence[i]] = {}
-		for item in pendingToTrade:
-			if pendingToTrade[item] != 0:
-				portfolio[item] += pendingToTrade[item]
-				PL[-1] -= (TransactionCost[item] * np.abs(pendingToTrade[item]))
-				transactionLog[dateSequence[i]][item] = pendingToTrade[item]
-				pendingToTrade[item] = 0
+				sellAmount = 1
 
-		### for performance estimation
-		if todayMaturity:
-			monthlyPL.append(PL[-1])
-			maturities.append(futures_data[i][futuresDateIdx])
+				if (-macd[i]) > proportion * R * fast[i]:
+					pendingToTrade['TX'] -= sellAmount
+	#				sellOptionFlag = True
+
+	#			if todayMaturity and sellOptionFlag:
+	#				sellOptionFlag = False
+
+				# stopping loss
+	#			if macd[i] > 0:
+	#				pendingToTrade['TX'] = -portfolio['TX']
+	#				pendingToTrade['TXO'] = -portfolio['TXO']
+
+
+				#	if i > 0 and macd[i-1] < 0 and convergence[i - 1] > 0 and convergence[i] < 0:
+				#		pendingToTrade['TXO'] -= shortAmount
+				#		shortFlag = True
+				#
+				#	if i > 0 and macd[i-1] > 0 and convergence[i - 1] < 0 and convergence[i] > 0:
+				#		pendingToTrade['TXO'] -= shortAmount
+				#		shortFlag = True
+
+				#	if i > 0 and convergence[i - 1] < 0 and convergence[i] > 0:
+				#		pendingToTrade['TXO'] -= portfolio['TXO']
+				#		shortFlag = False
+
+			########################################################################
+
+			### check futures / option matures
+			if todayMaturity:
+				pendingToTrade['TXO'] = -portfolio['TXO']
+				pendingToTrade['TX'] = -portfolio['TX']
+
+				priceDifference['TXO'][i + 1] = 0
+				priceDifference['TX'][i + 1] = 0
+
+			### validate transaction
+			for item in pendingToTrade:
+				if pendingToTrade[item] > 0 and portfolio[item] >= positionLimit[item]:
+					pendingToTrade[item] = 0
+
+				if pendingToTrade[item] < 0 and -portfolio[item] >= positionLimit[item]:
+					pendingToTrade[item] = 0
+
+			### execute transaction
+			transactionLog[dateSequence[i]] = {}
+			for item in pendingToTrade:
+				if pendingToTrade[item] != 0:
+					portfolio[item] += pendingToTrade[item]
+					PL[-1] -= (TransactionCost[item] * np.abs(pendingToTrade[item]))
+					transactionLog[dateSequence[i]][item] = pendingToTrade[item]
+					pendingToTrade[item] = 0
+
+			### for performance estimation
+			if todayMaturity:
+				monthlyPL.append(PL[-1])
+				maturities.append(futures_data[i][futuresDateIdx])
 
 #	print constant
 #	plt.plot(dateSequence[beginDateIndex:], PL[1:])
@@ -291,8 +292,8 @@ monthlyReturn = np.diff(monthlyPL) / initCapital
 
 axescolor  = '#ffffff'
 left, width = 0.1, 1.5
-rect1 = [left, 1.2*2, width, 0.6*2]
-rect2 = [left, 0.8*2, width, 0.4*2]
+rect1 = [left, 1.2*1.8, width, 0.6*1.8]
+rect2 = [left, 0.8*1.8, width, 0.4*1.8]
 fig = plt.figure(facecolor='white')
 
 ax1 = fig.add_axes(rect1, axisbg=axescolor)  #left, bottom, width, height
@@ -344,11 +345,14 @@ Parameter $R$ :  {10}%
 if not hedgeFlag:
 	strategyReportText += 'Proportion $p$ : {}%'.format(proportion*100)
 
-fig.text(1.93, 3.35, strategyReportText, ha='right', va='center')
+fig.text(1.93, 3.0, strategyReportText, ha='right', va='center')
 
 if hedgeFlag:
 	print( 'Strategy : Hedging' )
 	ax2.set_yticks([-R, R])
+#	fig.savefig('../Hedging_R{0}.png'.format(R))
 else:
 	print( 'Strategy : Aggressive Selling' )
 	ax2.set_yticks([-R, -proportion*R, proportion*R, R])
+#	fig.savefig('../Aggressive_R{0}_p{1}.png'.format(R, proportion))
+
